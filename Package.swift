2d493b034df90e57f3ef2bdf61e653302737097c
Andreas Link <andreas.link@jamitlabs.com>
2019-06-28 08:37:22 +0200

    Update swift package manager support

--- Package.swift
+++ Package.swift
@@ -1,18 +1,19 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftyUserDefaults",
+    platforms: [.iOS("8.0"), .macOS("10.11"), .tvOS("9.0"), .watchOS("2.0")],
     products: [
         .library(
             name: "SwiftyUserDefaults",
             targets: ["SwiftyUserDefaults"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "1.3.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "7.1.0"))
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.2"))
     ],
     targets: [
         .target(
